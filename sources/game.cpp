57e5539dd307ca89a9ffd72439cc7053fbe55a47
mukatrinm <mukatrinm@gmail.com>
2023-04-09 19:50:55 +0300

    fixed game logic

--- sources/game.cpp
+++ sources/game.cpp
@@ -58,9 +58,17 @@ void ariel::Game::playTurn() {
 
     // check if game is finished
     if (player1_.stacksize() == 0 && game_status_ != GameStatus::FINISHED) {
-        endGame(Winner::PLAYER_2);
-    } else if (player2_.stacksize() == 0 && game_status_ != GameStatus::FINISHED) {
-        endGame(Winner::PLAYER_1);
+        if (player2_.stacksize() != 0) {
+            throw std::runtime_error("game didn't run as expected");
+        }
+
+        if (player1_.cardesTaken() == player2_.cardesTaken()) {
+            endGame(Winner::TIE);
+        } else if (player1_.cardesTaken() > player2_.cardesTaken()) {
+            endGame(Winner::PLAYER_1);
+        } else {
+            endGame(Winner::PLAYER_2);
+        }
     }
     log_.push_back(turn_log_);
 }
@@ -73,79 +81,74 @@ void ariel::Game::war(Card& card1, Card& card2) {
 
     turn_log_ += " WAR! ";
 
-    if (player1_.stacksize() > 0) {
-        card1 = player1_.drawCard();
-        deck_.push_back(card1);
-    }
-    if (player2_.stacksize() > 0) {
-        card2 = player2_.drawCard();
-        deck_.push_back(card2);
-    }
-
-    if (deck_.size() == 2) {
+    if (player1_.stacksize() == 0 && deck_.size() == 0) {
+        // player1 and player2 had no more cards during a war.
+        // if e.g. 5, 5 -> they take back the 5, 5 and check who has more cards won (cards taken)
+        // if they have the same amount of cards taken, its a TIE
+        if (player1_.cardesTaken() == player2_.cardesTaken()) {
+            player1_.incCardsTaken();
+            player2_.incCardsTaken();
+            endGame(Winner::TIE);
+        } else if (player1_.cardesTaken() > player2_.cardesTaken()) {
+            player1_.incCardsTaken();
+            player1_.incCardsTaken();
+            endGame(Winner::PLAYER_1);
+        } else {
+            player2_.incCardsTaken();
+            player2_.incCardsTaken();
+            endGame(Winner::PLAYER_2);
+        }
+        return;
+    } else if (player1_.stacksize() == 0 && deck_.size() != 0) {
         // player1 and player2 had no more cards.
         // if e.g. 5, 5 -> they take back the 5, 5 and check who has more cards won (cards taken)
         // if they have the same amount of cards taken, its a TIE
-        if (player1_.stacksize() == 0 && player2_.stacksize() == 0) {
-            if (player1_.cardesTaken() == player2_.cardesTaken()) {
-                player1_.incCardsTaken();
-                player2_.incCardsTaken();
-                endGame(Winner::TIE);
-            } else if (player1_.cardesTaken() > player2_.cardesTaken()) {
-                player1_.incCardsTaken();
-                player1_.incCardsTaken();
-                endGame(Winner::PLAYER_1);
-            } else {
-                player2_.incCardsTaken();
-                player2_.incCardsTaken();
-                endGame(Winner::PLAYER_2);
-            }
-        } else {
-            // shuffle and split the cards played in this turn between the players. always even number
-            auto rd = std::random_device{};
-            auto rng = std::default_random_engine{rd()};
-            std::shuffle(deck_.begin(), deck_.end(), rng);
+        auto rd = std::random_device{};
+        auto rng = std::default_random_engine{rd()};
+        std::shuffle(deck_.begin(), deck_.end(), rng);
 
-            // split the deck between the players
-            for (unsigned int i = 0; i < deck_.size() / 2; ++i) {
-                player1_.addCard(deck_[i]);
-            }
-            for (unsigned int i = deck_.size() / 2; i < deck_.size(); ++i) {
-                player2_.addCard(deck_[i]);
-            }
+        // split the deck between the players
+        for (unsigned int i = 0; i < deck_.size() / 2; ++i) {
+            player1_.addCard(deck_[i]);
         }
-    } else if (deck_.size() < 4) { 
-        // if one of the players doesn't have enough cards for war, the other player wins
-        if (player1_.stacksize() == 0) {
-            turn_log_ += " player " + player2_.getName() + " wins.";
-            endGame(Winner::PLAYER_2);
-        } else {
-            turn_log_ += " player " + player1_.getName() + " wins.";
-            endGame(Winner::PLAYER_1);
+        for (unsigned int i = deck_.size() / 2; i < deck_.size(); ++i) {
+            player2_.addCard(deck_[i]);
         }
+
         return;
     }
+
+    // they play hidden cards
+    card1 = player1_.drawCard();
+    deck_.push_back(card1);
+
+    card2 = player2_.drawCard();
+    deck_.push_back(card2);
 
     if (player1_.stacksize() > 0) {
         card1 = player1_.drawCard();
         turn_log_ += player1_.getName() + " played " + card1.toString() + ", ";
         deck_.push_back(card1);
-    }
-    if (player2_.stacksize() > 0) {
+
         card2 = player2_.drawCard();
         turn_log_ += player2_.getName() + " played " + card2.toString() + ", ";
         deck_.push_back(card2);
-    }
+    } else {
+        // player1 and player2 have no more cards for the war.
+        // shuffle and split thrown cards.
 
-    if (deck_.size() < 6) { 
-        // if one of the players doesn't have enough cards, the other player wins
-        if (player1_.stacksize() == 0) {
-            turn_log_ += " player " + player2_.getName() + " wins.";
-            endGame(Winner::PLAYER_2);
-        } else {
-            turn_log_ += " player " + player1_.getName() + " wins.";
-            endGame(Winner::PLAYER_1);
+        auto rd = std::random_device{};
+        auto rng = std::default_random_engine{rd()};
+        std::shuffle(deck_.begin(), deck_.end(), rng);
+
+        // split the deck between the players
+        for (unsigned int i = 0; i < deck_.size() / 2; ++i) {
+            player1_.addCard(deck_[i]);
         }
+        for (unsigned int i = deck_.size() / 2; i < deck_.size(); ++i) {
+            player2_.addCard(deck_[i]);
+        }
+
         return;
     }
 
@@ -206,9 +209,7 @@ void ariel::Game::endGame(Winner winner) {
         name_of_winner_ = player1_.getName();
     } else if (winner == Winner::PLAYER_2) {
         name_of_winner_ = player2_.getName();
-    } else {
-        name_of_winner_ = nullptr;
-    }
+    }  // else it's a tie, name_of_winner_ should stay empty.
 
     game_status_ = GameStatus::FINISHED;
 }
